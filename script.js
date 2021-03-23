f13976fade9d0bf4ad6062fd60fdf0649d09c99f
Roman Princ <u19.421@pslib.cloud>
2021-03-23 14:03:22 +0100

    jump

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function jump(){
 var checkDead = setInterval(function(){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>0 && characterTop>=130){
+    if(blockLeft<20 && blockLeft>0 && characterTop>=110){
         block.style.animation = "none";
         block.style.display = "none";
         alert("Prohrál jsi! Pro nový pokus refreshni stránku")
